254b5d71174a00cc8668ff21f8f6e34e9c215f6b
lukashenko-work <lukashenko.work@mail.ru>
2026-04-10 09:16:29 +0300

    feat: add Product, Category, Order, OrderItem, and Review models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from products.models import Product
+
+User = get_user_model()
+
+class Order(models.Model):
+    
+    
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Ожидает'
+        PAID = 'paid', 'Оплачен'
+        SHIPPED = 'shipped', 'Отправлен'
+        DELIVERED = 'delivered', 'Доставлен'
+        CANCELLED = 'cancelled', 'Отменен'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders', verbose_name='Пользователь')
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING, verbose_name='Статус')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Итоговая сумма')
+    shipping_address = models.TextField(verbose_name='Адрес доставки')
+    full_name = models.CharField(max_length=200, verbose_name='Получатель')
+    email = models.EmailField(verbose_name='E-mail')
+    phone_number = models.CharField(max_length=20, verbose_name='Телефон')
+    comment = models.TextField(blank=True, null=True, verbose_name='Комментарий')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self._meta.verbose_name} №{self.pk} - {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name='Заказ')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items', verbose_name='Товар')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Кол-во')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')  # Цена на момент заказа
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Позиция заказа'
+        verbose_name_plural = 'Позиции заказа'
+
+    def __str__(self):
+        return f'{self._meta.verbose_name} №{self.order.pk} {self.product.name} x {self.quantity}'
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from products.models import Product
 
 # Create your models here.
+
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', verbose_name='Пользователь')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews', verbose_name='Товар')
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name='Рейтинг')
+    comment = models.TextField(verbose_name='Текст отзыва')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_ar = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        unique_together = ('user', 'product')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self._meta.verbose_name} - {self.user.username} - {self.product.name} {self.rating}/5'
