678702d8d95bd360a506e3a911e76ce6c3942388
lukashenko-work <lukashenko.work@mail.ru>
2026-04-30 13:47:50 +0300

    feat: add ajax cart view and enable black formatter

--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.db import transaction
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.views.generic import DetailView
@@ -13,14 +13,14 @@ from django.views.generic.base import TemplateView
 
 from core import settings
 from orders.models import Order, OrderItem
+from products.models import Product
 
 from .cart import Cart
 from .forms import CheckoutForm
-from products.models import Product
 
 
 class CartAddView(View):
-    def post(self, request: HttpRequest,  product_id: int):
+    def post(self, request: HttpRequest, product_id: int):
         product = get_object_or_404(Product, id=product_id, is_active=True)
         cart = Cart(request)
         quantity = int(request.POST.get('quantity', 1))
@@ -32,6 +32,31 @@ class CartAddView(View):
         messages.success(request, message=f'{product.name} добавлен в корзину')
 
         return redirect('orders:cart')
+
+
+class CartAddViewAJAX(View):
+    def post(self, request: HttpRequest, product_id: int):
+        product = get_object_or_404(Product, id=product_id, is_active=True)
+        print(product)
+        cart = Cart(request)
+        print(cart)
+        quantity = int(request.POST.get('quantity', 1))
+
+        if quantity < 1 or quantity > product.stock:
+            messages.error(
+                request,
+                message=f'Недопустимое количество. В наличии {product.stock} шт.',
+            )
+            return JsonResponse({'error': 'Неверные данные'}, status=400)
+
+        cart.add(product, quantity)
+        messages.success(request, message=f'{product.name} добавлен в корзину')
+
+        return JsonResponse({'status': 'success'})
+
+
+def is_ajax(request):
+    return request.headers.get('x-requested-with') == 'XMLHttpRequest'
 
 
 class CartRemoveView(View):
@@ -75,11 +100,8 @@ class CheckoutView(LoginRequiredMixin, View):
 
         form = CheckoutForm()
         total = cart.get_total_price()
-        return render(request, template_name, {
-            'form': form,
-            'cart': cart,
-            'total': total
-        })
+        return render(request, template_name, {'form': form, 'cart': cart, 'total': total}
+        )
 
     def post(self, request: HttpRequest) -> HttpResponse:
         """Создание заказа"""
@@ -91,11 +113,11 @@ class CheckoutView(LoginRequiredMixin, View):
         form = CheckoutForm(request.POST)
 
         if not form.is_valid():
-            return render(request, 'orders/checkout.html', {
-                'form': form,
-                'cart': cart,
-                'total': cart.get_total_price()
-            })
+            return render(
+                request,
+                'orders/checkout.html',
+                {'form': form, 'cart': cart, 'total': cart.get_total_price()},
+            )
 
         with transaction.atomic():
             order = Order.objects.create(
@@ -103,7 +125,7 @@ class CheckoutView(LoginRequiredMixin, View):
                 full_name=form.cleaned_data['full_name'],
                 email=form.cleaned_data['email'],
                 phone_number=form.cleaned_data['phone_number'],
-                shipping_address=f"{form.cleaned_data['zip_code']}, {form.cleaned_data['city']}, {form.cleaned_data['address']}",
+                shipping_address=f'{form.cleaned_data["zip_code"]}, {form.cleaned_data["city"]}, {form.cleaned_data["address"]}',
                 # city=form.cleaned_data['city'],
                 # zip_code=form.cleaned_data['zip_code'],
                 comment=form.cleaned_data['comment'],
@@ -118,7 +140,9 @@ class CheckoutView(LoginRequiredMixin, View):
                     # Откат транзакции
                     raise ValueError(f'Недостаточно {product.name} на складе')
 
-                OrderItem.objects.create(order=order, product=product, quantity=qty, price=item['price'])
+                OrderItem.objects.create(
+                    order=order, product=product, quantity=qty, price=item['price']
+                )
 
                 # Select for update
                 Product.objects.filter(id=product.id).update(stock=product.stock - qty)
@@ -126,7 +150,7 @@ class CheckoutView(LoginRequiredMixin, View):
         cart.clear()
 
         self._send_confirmation_email(order)
-        message = f"Заказ №{order.pk} успешно оформлен. Спасибо за покупку!"
+        message = f'Заказ №{order.pk} успешно оформлен. Спасибо за покупку!'
         messages.success(request, message)
 
         return redirect('orders:success', order_id=order.pk)
@@ -169,7 +193,7 @@ class CheckoutView(LoginRequiredMixin, View):
 
 class OrderSuccessView(LoginRequiredMixin, DetailView):
     model = Order
-    template_name = "orders/success.html"
+    template_name = 'orders/success.html'
     context_object_name = 'order'
 
     pk_url_kwarg = 'order_id'
